aa8b3f4dc7027cc04a107251d6d2863a669e244c
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-31 23:36:38 +0530

    Added bubble sort

--- src/com/bridgelabz/BubbleSort.java
+++ src/com/bridgelabz/BubbleSort.java
@@ -1,34 +1,33 @@
 package com.bridgelabz;
 
 public class BubbleSort {
-        public void sortArray(int[] numArray) {
-            int n = numArray.length;
-            int temp = 0;
+    public void sortArray(int[] numArray) {
+        int n = numArray.length;
+        int temp = 0;
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 1; j < (n - i); j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
 
-                    if (numArray[j - 1] > numArray[j]) {
-                        temp = numArray[j - 1];
-                        numArray[j - 1] = numArray[j];
-                        numArray[j] = temp;
-                    }
-
+                if (numArray[j - 1] > numArray[j]) {
+                    temp = numArray[j - 1];
+                    numArray[j - 1] = numArray[j];
+                    numArray[j] = temp;
                 }
+
             }
         }
+    }
 
-        public void printArray(int[] x) {
-            for(int i=0; i<x.length; i++)
-                System.out.print(x[i] + " ");
-        }
+    public void printArray(int[] x) {
+        for(int i=0; i<x.length; i++)
+            System.out.print(x[i] + " ");
+    }
 
-        public static void main(String[] args) {
-            BubbleSort b = new BubbleSort();
-            int[] num = {50,48,37,52,18};
-            System.out.println("Sorted List:");
-            b.sortArray(num);
-            b.printArray(num);
-        }
+    public static void main(String[] args) {
+        BubbleSort b = new BubbleSort();
+        int[] num = {50,48,37,52,18};
+        System.out.println("Sorted List:");
+        b.sortArray(num);
+        b.printArray(num);
+    }
 }
-
